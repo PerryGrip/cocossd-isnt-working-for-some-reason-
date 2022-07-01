2cdf54fdaeb1e1e9d7bddb97083f69aefb3fcb1c
PerryGrip <97786175+PerryGrip@users.noreply.github.com>
2022-07-01 17:30:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+img = "";
+Status = "";
+function preload(){
+    img = loadImage('dog_cat.jpg');
+}
+function setup(){
+    canvas = createCanvas( 640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("Status").innerHTML = "Status = Detecting Objects";
+}
+//
+function draw(){
+    image(img, 0, 0, 640, 420);
+    fill("#FF0000")
+    text("dog", 45, 75);
+    noFill();
+    stroke("#FF0000");
+    rect(30, 60, 450, 350);
+
+    fill("#FF0000");
+    text("cat", 320, 120);
+    noFill();
+    stroke("#FF0000");
+    rect(300, 90, 270, 320);
+}
+function modelLoaded(){
+    console.log("model Loaded!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+}
